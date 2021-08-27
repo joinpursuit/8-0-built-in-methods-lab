c9b4187f7d269a89d1d1243a7695181470d51b69
Cristobal Arias <cristobal.arias.13@gmail.com>
2021-08-27 14:31:41 -0400

    Completed lab

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments);
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +14,21 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  let receivedAndMax = Object.values(score);
+  let percentageDecimal = receivedAndMax[0] / receivedAndMax[1];
+
+  return Math.round(percentageDecimal * 100);
+
+  // let percentageDecimal = score.received / score.max;
+
+  // return Math.round(percentageDecimal * 100);
+
+  //score received, divided by 100
+  //separate line multiplying that result by 100 to turn it into an integer.
+
+  //open question: I'm wondering if the JS Doc actually meant that it wants us to have three parameters. I ended up only using one parameter for the function.
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +38,9 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  return Math.max(getPercentageScore(score1), getPercentageScore(score2));
+}
 
 // Don't change anything below this line.
 module.exports = {
