fd4fa892b69c1a6ac7a2921825f662eb4a7196f9
salrey <tekdehn@gmail.com>
2021-08-27 12:16:18 -0400

    Add solutions to built-in methods functions

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const assignmentsData = require("./data.js").assignments;
+
 /**
  * Returns all of the "categories" from the assignments object.
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments);
+};
+
+//Check
+// console.log(assignmentsData);
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +19,12 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  return Math.round(score.received/score.max * 100);
+};
+
+//check
+// console.log(getPercentageScore(assignmentsData.quizzes[1].score, assignmentsData.quizzes[1].score.received, assignmentsData.quizzes[1].score.max))
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +34,12 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  return Math.max(getPercentageScore(score1), getPercentageScore(score2));
+};
+
+console.log(getHighestOfTwoScores(assignmentsData.projects[0].score,
+  assignmentsData.projects[1].score))
 
 // Don't change anything below this line.
 module.exports = {
