d947e5233e08ad5fe0e32909d518a5ff6151b6d3
salrey <tekdehn@gmail.com>
2021-08-27 12:32:22 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+const assignments = {
+    quizzes: [
+      { title: "Arrays", score: { received: 4, max: 5 } },
+      { title: "Objects", score: { received: 5, max: 6 } },
+      { title: "Loops", score: { received: 2, max: 4 } },
+    ],
+    labs: [
+      { title: "Arrays", score: { received: 0, max: 1 } },
+      { title: "Objects", score: { received: 1, max: 1 } },
+      { title: "Loops", score: { received: 1, max: 1 } },
+    ],
+    projects: [
+      { title: "Music Box", score: { received: 14, max: 18 } },
+      { title: "Calculator", score: { received: 12, max: 12 } },
+    ],
+  };
+
+  module.exports = {
+    assignments,
+  }
