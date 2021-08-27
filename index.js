7d8b0a4a498b8eaa06c94bcde6bf3f36a4e115bb
Milton Clark <kingcoder247@gmail.com>
2021-08-27 14:20:26 -0400

    All functions passed tests

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments);
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +14,33 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+
+//  const assignments = {
+//   quizzes: [
+//     { title: "Arrays", score: { received: 4, max: 5 } },
+//     { title: "Objects", score: { received: 5, max: 6 } },
+//     { title: "Loops", score: { received: 2, max: 4 } },
+//   ],
+//   labs: [
+//     { title: "Arrays", score: { received: 0, max: 1 } },
+//     { title: "Objects", score: { received: 1, max: 1 } },
+//     { title: "Loops", score: { received: 1, max: 1 } },
+//   ],
+//   projects: [
+//     { title: "Music Box", score: { received: 14, max: 18 } },
+//     { title: "Calculator", score: { received: 12, max: 12 } },
+//   ],
+// };
+
+
+
+
+
+
+function getPercentageScore(score) {
+  return Math.round((score.received/score.max)*100);
+
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +50,11 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+let percentageScore1 = Math.round((score1.received/score1.max)*100);
+let percentageScore2 = Math.round((score2.received/score2.max)*100);
+return Math.max(percentageScore1, percentageScore2);
+}
 
 // Don't change anything below this line.
 module.exports = {
