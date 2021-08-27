71e8804d780290998cc8c3b60989bae312a1b2b2
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-08-27 12:49:53 -0400

    problems solved with Lakisha

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  //return category name as an array string
+  return Object.keys(assignments)
+
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +16,15 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  //return a number with a ROUNDED integer...
+  let percentage = score.received / score.max * 100; //basic percentage formula
+
+  return Math.round(percentage);
+
+
+
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +34,11 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  //returning highest num = Math.max?
+
+  return Math.max(score1)
+}
 
 // Don't change anything below this line.
 module.exports = {
