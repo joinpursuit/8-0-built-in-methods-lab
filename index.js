8a4ccdfce677e1cd55d257bfd62804757e0a56ba
Bryant Mercedes <bryantmercedescanela@gmail.com>
2021-08-27 17:47:54 -0400

    Finished

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments);}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +13,10 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  let percentage = (score.received / score.max) * 100
+  return Math.round(percentage)
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +26,20 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let firstScore = (score1.received / score1.max) * 100
+  let firstPercentage = Math.round(firstScore)
+  let secondScore = (score2.received / score2.max) * 100
+  let secondPercentage = Math.round(secondScore)
+
+  let smartest = firstPercentage
+
+  if (secondPercentage > smartest){
+    smartest = secondPercentage
+  }
+
+return smartest
+}
 
 // Don't change anything below this line.
 module.exports = {
