0c8a395e074505cbef3df25db62657e8f93c2521
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-08-27 12:31:19 -0400

    completes highest of two

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ function getPercentageScore(score) {
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+ let first = Math.round(score1.received / score1.max * 100);
+ let second = Math.round(score2.received / score2.max * 100);
+ return Math.max(first, second)
+}
 
 // Don't change anything below this line.
 module.exports = {
