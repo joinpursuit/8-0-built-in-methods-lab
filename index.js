1c2726d52568f0543267be765ff74e6e59b2e5bf
Eva Rodriguez <evarodriguez@Evas-MacBook-Pro.local>
2021-08-27 13:35:16 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments);
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +14,13 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+
+function getPercentageScore(score) { 
+
+let result = {}
+result = Math.round((score.received / score.max)  * 100)
+return result;
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +30,19 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+
+function getHighestOfTwoScores(score1, score2) {
+let newObj = {}
+newObj = Math.round((score1.received / score1.max)  * 100)
+
+let newObj2 = {}
+newObj2 = Math.round((score2.received / score2.max)  * 100)
+
+return Math.max(newObj, newObj2)
+}
+
+// this is a different solution calling getPercentageScore function!! both solutions worked!
+// return Math.max(getPercentageScore(score1), getPercentageScore(score2));}
 
 // Don't change anything below this line.
 module.exports = {
