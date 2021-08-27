0aa55bc6a31ed5e4b178342f665ae31527ed28b4
theodorefrazier <theo.frazier@icloud.com>
2021-08-27 15:46:47 -0400

    finished - partner Kiyomi

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+
+  return Object.keys(assignments)
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +15,12 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+
+  return Math.round(score.received * 100 / score.max)
+
+}
+
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +30,16 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+
+  let scoreOne = Math.round(score1.received * 100/score1.max)
+  let scoreTwo = Math.round(score2.received * 100/score2.max)
+
+
+  return Math.max(scoreOne, scoreTwo)
+}
+//NOTE TO SELF - BE MINDFUL OF SPELLING! One 'received' was spelled wrong! Making all the tests fail!
+
 
 // Don't change anything below this line.
 module.exports = {
