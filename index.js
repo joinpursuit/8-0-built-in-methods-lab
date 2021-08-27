62b75f2612a9ea80d3575f5e3a957a83b872603a
Fatema Kabir <fatema.kabir@pursuit.org>
2021-08-27 14:23:02 -0400

    built-in-methods

--- index.js
+++ index.js
@@ -3,16 +3,20 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments)
+}
 
 /**
- * Returns an integer that represents the score as a percentage.
+ * Returns an .
  * @param {object} score - An object representing the score of an assignment.
  * @param {number} score.received - The value received on the assignment.
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  return Math.round((score.received / score.max * 100))
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +26,17 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  //let highOfScore1 = Math.round((score1.received / score1.max * 100))
+  //get the percentage value from object score1
+  //let highOfScore2 = Math.round((score2.received / score2.max * 100))
+  //get the percentage value from object score2
+  let highOfScore1 = getPercentageScore(score1)
+  let highOfScore2 = getPercentageScore(score2)
+  //get the highest value from the two objects
+  return Math.max(highOfScore1,highOfScore2)
+}
+  
 
 // Don't change anything below this line.
 module.exports = {
