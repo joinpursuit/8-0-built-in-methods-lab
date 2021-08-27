be554c3959870c6acbb1620377a5de3cde1d1b09
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-08-27 12:38:38 -0400

    Solution to these problems

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+ /*
+ const assignments = {
+   quizzes: [
+     { title: "Arrays", score: { received: 4, max: 5 } },
+     { title: "Objects", score: { received: 5, max: 6 } },
+     { title: "Loops", score: { received: 2, max: 4 } },
+   ],
+   labs: [
+     { title: "Arrays", score: { received: 0, max: 1 } },
+     { title: "Objects", score: { received: 1, max: 1 } },
+     { title: "Loops", score: { received: 1, max: 1 } },
+   ],
+   projects: [
+     { title: "Music Box", score: { received: 14, max: 18 } },
+     { title: "Calculator", score: { received: 12, max: 12 } },
+   ],
+ };
+ */
+
+
 /**
  * Returns all of the "categories" from the assignments object.
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments);
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +35,9 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  return Math.round(score.received*100/score.max);
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +47,9 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  return Math.max(getPercentageScore(score1), getPercentageScore(score2));
+}
 
 // Don't change anything below this line.
 module.exports = {
