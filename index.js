fa30940a822468abfd3922bc3a657c4ede823467
Cesar Ortiz <ceazthemoment@aol.com>
2021-08-29 23:57:21 -0400

    finished lab

--- index.js
+++ index.js
@@ -14,7 +14,9 @@
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+ function getPercentageScore(score) {
+  return (Math.round(score.received/score.max*100))
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -24,7 +26,9 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+funfunction getHighestOfTwoScores(score1, score2) {
+  return(Math.max(getPercentageScore(score1),getPercentageScore(score2)))
+}ction getHighestOfTwoScores(score1, score2) {}
 
 // Don't change anything below this line.
 module.exports = {
