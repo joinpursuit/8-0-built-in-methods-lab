6ff39b8da8cc45f2bd8215cf77a28f52810297c7
arupay <arupay11374@gmail.com>
2022-02-28 14:26:26 -0500

    Done

--- index.js
+++ index.js
@@ -7,22 +7,6 @@ function getAssignmentTypes(assignments) {
   let x = Object.keys(assignments);
   return x;
 }
-// const assignments = {
-//   quizzes: [
-//     { title: "Arrays", score: { received: 4, max: 5 } },
-//     { title: "Objects", score: { received: 5, max: 6 } },
-//     { title: "Loops", score: { received: 2, max: 4 } },
-//   ],
-//   labs: [
-//     { title: "Arrays", score: { received: 0, max: 1 } },
-//     { title: "Objects", score: { received: 1, max: 1 } },
-//     { title: "Loops", score: { received: 1, max: 1 } },
-//   ],
-//   projects: [
-//     { title: "Music Box", score: { received: 14, max: 18 } },
-//     { title: "Calculator", score: { received: 12, max: 12 } },
-//   ],
-// };
 
 /**
  * Returns an integer that represents the score as a percentage.
