fa9a529bff9760e6ce81753a152901591073888a
Victor E Lopez <velbx51594@macbook-air.myfiosgateway.com>
2022-03-02 21:46:17 -0500

    8-0 built methods lab

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+   return Object.keys(assignments)
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +14,13 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  let scores = Object.values(score);
+  
+  for (let i = 0; i < scores.length; i++) {
+    return Number(((scores[0] / scores[1]) * 100).toFixed())
+  } 
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +30,22 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let scores1 = Object.values(score1);
+  let scores2 = Object.values(score2);
+  let maxScore1 = 0;
+  let maxScore2 = 0;
+  for (let i = 0; i < scores1.length; i++) {
+     maxScore1 = Math.max(Number(((scores1[0] / scores1[1]) * 100).toFixed()))
+  } 
+  for (let j = 0; j < scores2.length; j++) {
+     maxScore2 =  Math.max(Number(((scores2[0] / scores2[1]) * 100).toFixed()))
+  }
+  return (maxScore1 > maxScore2 ? maxScore1 : maxScore2)
+}
+
+
+
 
 // Don't change anything below this line.
 module.exports = {
