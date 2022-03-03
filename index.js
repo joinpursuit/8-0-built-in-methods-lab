d96332e13d6fe11ea391765963315d706a569cf4
Shawn Faber <sfaber02@gmail.com>
2022-03-02 20:15:30 -0500

    finished built in methods lab

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments);
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +14,9 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  return Math.round (score.received * 100 / score.max);
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +26,11 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  const score1Percent = Math.round (score1.received * 100 / score1.max);
+  const score2Percent = Math.round (score2.received * 100 / score2.max);
+  return score1Percent > score2Percent ? score1Percent : score2Percent;
+}
 
 // Don't change anything below this line.
 module.exports = {
