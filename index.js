79023fffdfd9cd745496d5c02728c418c85898bb
ziqian pan <ziqianpan1998@gmail.com>
2022-03-02 20:38:17 -0500

    finished lab

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+let result = Object.keys(assignments);
+
+//result.push(Object.keys(assignments));
+
+//console.log(Object.keys(assignments));
+
+return result;
+
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +21,13 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+let result = Math.round(score.received/score.max *100);
+
+//console.log(score.received/score.max);
+
+return result;
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +37,19 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let result = 0;
+  let score1Perc = Math.round(score1.received/score1.max *100);
+  let score2Perc  = Math.round(score2.received/score2.max *100);
+  //console.log(score1Perc);
+  if (score1Perc > score2Perc){
+    result = score1Perc;
+  } else {
+    result = score2Perc;
+  }
+
+return result;
+}
 
 // Don't change anything below this line.
 module.exports = {
