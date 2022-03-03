91307aebb36203c5fe7513f021e72a135615c5f8
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-03-02 22:25:20 -0500

    Built In assignment

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+
+ function getAssignmentTypes(assignments) {
+  return Object.keys(assignments)
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +15,11 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+   let values = Object.values(score);
+   let finP = values[0] / values[1] * 100;
+   return Math.round(finP)
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +29,13 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let values1 = Object.values(score1);
+  let values2 = Object.values(score2);
+   let finP1 = Math.round(values1[0] / values1[1] * 100);
+   let finP2 = Math.round(values2[0] / values2[1] * 100);
+   return Math.max(finP1,finP2);
+}
 
 // Don't change anything below this line.
 module.exports = {
