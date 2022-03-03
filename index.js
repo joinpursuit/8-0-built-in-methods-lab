0bae0549a9ca03adff3a6eab2a8f9cff89f3defb
Michael Kleemoff <michaelkleemoff2@gmail.com>
2022-03-03 14:57:01 -0500

    Create 2 spaces for better readability.

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function getAssignmentTypes(assignments) {
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
+
 function getPercentageScore(score) {
   const result =  Math.round((score.received / score.max) * 100);
   return result;
@@ -32,6 +33,7 @@ function getPercentageScore(score) {
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
+
 function getHighestOfTwoScores(score1, score2) {
   let result1 = Math.round((score1.received / score1.max) * 100);
   let result2 = Math.round((score2.received / score2.max) * 100);
