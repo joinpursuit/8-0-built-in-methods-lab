4006bc998808e4dad89d91d7237626cbc8213a74
masmei <masonmei@pursuit.org>
2022-03-02 19:40:14 -0600

    finished lab

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments)
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +14,12 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  let scoreValues = (Object.values(score))
+  let finalScore = scoreValues[0] / scoreValues[1] * 100
+  // console.log(finalScore)
+  return Math.round(finalScore)
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +29,17 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  
+  let scoreOneValues = (Object.values(score1))
+  let finalScoreOne = Math.round(scoreOneValues[0] / scoreOneValues[1] * 100)
+
+  let scoreTwoValues = (Object.values(score2))
+  let finalScoreTwo = Math.round(scoreTwoValues[0] / scoreTwoValues[1] * 100)
+
+  return Math.max(finalScoreOne, finalScoreTwo)
+
+}
 
 // Don't change anything below this line.
 module.exports = {
