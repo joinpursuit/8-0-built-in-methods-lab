1508da3ab15079796269272a3efb8c8c088ee64e
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-07-25 13:58:01 -0400

    passing 7/8

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments);
+}
+
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +15,12 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  let scoreResult = score.received / score.max
+  let scorePercent = (scoreResult * 100).toFixed(0)
+  console.log(scorePercent)
+  return Number(scorePercent)
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +30,20 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let scoreResult1 = score1.received / score1.max
+  let scorePercent1 = (scoreResult1 * 100).toFixed(0)
+
+  let scoreResult2 = score2.received / score2.max
+  let scorePercent2 = (scoreResult2 * 100).toFixed(0)
+
+  console.log(scorePercent1)
+  if (scorePercent1 > scorePercent2){
+    return Number(scorePercent1)
+  }else (scorePercent2 > scorePercent1) {
+    return Number(scorePercent2)
+  }
+}
 
 // Don't change anything below this line.
 module.exports = {
