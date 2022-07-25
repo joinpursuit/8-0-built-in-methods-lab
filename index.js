5c2afd8a53a5c06ebafa8a21533bb80a6cc4b046
i-guerrero <isaacguerrero@pursuit.org>
2022-07-25 17:40:14 -0400

    Add getHighestOfTwoScores to index.js

--- index.js
+++ index.js
@@ -26,7 +26,13 @@ function getPercentageScore(score) {
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  score1Percentage = Math.round((score1.received / score1.max) * 100);
+  score2Percentage = Math.round((score2.received / score2.max) * 100);
+  return score1Percentage > score2Percentage
+    ? score1Percentage
+    : score2Percentage;
+}
 
 // Don't change anything below this line.
 module.exports = {
