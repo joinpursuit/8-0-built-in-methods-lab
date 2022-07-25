3e1db03ca32e13ebd6f37296bfb6f08e3d6b2cdc
TrianePeart <TrianePeart@gmail.com>
2022-07-25 13:50:35 -0400

    Final Commit

--- index.js
+++ index.js
@@ -24,11 +24,24 @@ function getPercentageScore(score) {
  * Given two score objects, returns an integer that represents the highest percentage received.
  * @param {object} score1 - An object representing the score of an assignment.
  * @param {number} score1.received - The value received on the assignment.
+ * score2.received can be pulled from data 
  * @param {number} score1.max - The maximum amount of points that could be received on the assignment.
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let firstScore = getPercentageScore(score1)
+  let secondScore = getPercentageScore(score2)
+  //Pulling from the already created function to utilize the params. 
+  return Math.max(firstScore, secondScore)
+
+
+  /*
+  ALTERNATIVE CODE: 
+  let sc0re1 = score1.received/score1.max * 100
+  let sc0re2 = score2.received/score2.max * 100
+  */
+}
 
 // Don't change anything below this line.
 module.exports = {
