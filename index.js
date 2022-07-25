6277dd8539cd8a1ac0ef64c90527233efd7c15a5
chrisking718 <chrisking@pursuit.org>
2022-07-25 17:27:06 -0400

    8 passed test

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const assignments = {
+  quizzes: [
+    { title: "Arrays", score: { received: 4, max: 5 } },
+    { title: "Objects", score: { received: 5, max: 6 } },
+    { title: "Loops", score: { received: 2, max: 4 } },
+  ],
+  labs: [
+    { title: "Arrays", score: { received: 0, max: 1 } },
+    { title: "Objects", score: { received: 1, max: 1 } },
+    { title: "Loops", score: { received: 1, max: 1 } },
+  ],
+  projects: [
+    { title: "Music Box", score: { received: 14, max: 18 } },
+    { title: "Calculator", score: { received: 12, max: 12 } },
+  ],
+};
 /**
  * Returns all of the "categories" from the assignments object.
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments)
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,8 +30,10 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
-
+function getPercentageScore(score) {
+ return Math.round((score.received/score.max) * 100)
+}
+getPercentageScore(assignments)
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
  * @param {object} score1 - An object representing the score of an assignment.
@@ -22,7 +42,11 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let sc1 = Math.floor((score1.received /score1.max)*100)
+  let sc2 = Math.floor((score2.received /score2.max)*100)
+  return (sc1,sc2)
+}
 
 // Don't change anything below this line.
 module.exports = {
