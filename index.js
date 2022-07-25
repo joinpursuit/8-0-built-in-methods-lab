9b49c76699c78a6cfad800348f79cec376bd1c38
Yiannar <yiannarichey@pursuit.org>
2022-07-25 14:25:15 -0400

    inital commit

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  let x = Object.keys(assignments)
+  return x 
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +15,16 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  // how to return an integer as a percentage, representing the score?
+
+let x= (score.received/score.max)*100
+let y = Math.round(x)
+return y 
+  
+  // Math.max 
+  // Math.round
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +34,22 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  //math.ceil 
+
+  // how can we compare score 1 and score 2?
+
+  // should return the highest percentage value from two scores
+
+let x = (score1.received/score1.max)*100 // gives first score 
+let y = (score2.received/score2.max)*100 // gives second score 
+ 
+if ( x > y){
+  return Math.round(x)
+} else {
+  return Math.round(y) 
+}
+}
 
 // Don't change anything below this line.
 module.exports = {
