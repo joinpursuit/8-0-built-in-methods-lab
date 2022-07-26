9ead605bc214a00c13f1b7a1933b7175b714aed3
SasukeDaTony <tonytaylor@pursuit.org>
2022-07-26 17:03:59 -0400

    Highest-Score-Function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function getAssignmentTypes(assignments) {
  * @returns {number} A rounded integer, representing a percentage.
  */
  function getPercentageScore(score) {
-  return Math.round((score.received/score.max) * 100)
+  return Math.round((score.received/score.max) * 100);
   }
 
 /**
@@ -26,7 +26,9 @@ function getAssignmentTypes(assignments) {
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  return Math.max(Math.round((score1.received/score1.max) * 100), Math.round((score2.received/score2.max) * 100))
+}
 
 // Don't change anything below this line.
 module.exports = {
