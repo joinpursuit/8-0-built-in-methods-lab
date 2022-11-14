e7538c040c2ae7de6297e006bd8752e61e96d45f
HaroldF415 <harold.f415@gmail.com>
2022-11-14 12:12:59 -0500

    getHighestOfTwoScores() complete

--- index.js
+++ index.js
@@ -31,7 +31,11 @@
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+ function getHighestOfTwoScores( score1, score2 ) {
+
+  return Math.max( getPercentageScore( score1 ), getPercentageScore( score2 ) );
+ 
+ } // ends getHighestOfTwoScore()
 
 // Don't change anything below this line.
 module.exports = {
