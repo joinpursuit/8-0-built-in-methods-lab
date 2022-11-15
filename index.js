c590a1381c89bd25180e5b2e08c3e7627e0e40bf
Wilsantos1975 <Faustosantos@pursuit.org>
2022-11-14 21:32:20 -0500

    finish lab

--- index.js
+++ index.js
@@ -26,7 +26,17 @@ function getPercentageScore(score) {
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  return Math.max(Math.round((score1.received /score1.max) * 100)), Math.round((score2.received /score2.max) * 100)
+  
+  // if (score1.received > score2.received) {
+  //   return Math.max(Math.round((score1.received /score1.max) * 100))
+  // } else if (score1.received < score2.received) {
+  //   return Math.max(Math.round((score2.received /score2.max) * 100))
+  // }
+}
+
+
 
 // Don't change anything below this line.
 module.exports = {
