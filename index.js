27493aafcd8b421905f0ad55922bae711e78d982
christinacephus@pursuit.org <christinacephus@pursuit.org>
2022-11-14 21:35:59 -0500

    teamlucky7 invoked a function

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  //we only want all the keys in the assignments object
+  return Object.keys(assignments)
+  //console.log(Object.keys(assignments))
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +16,9 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  return Math.round((score.received / score.max) * 100)
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +28,11 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  //these values are already recieved , score2 value is mirroring score1
+  //and because we are invoking the previous function we already got the rounded value set as well as the percentage scores to compare
+  return Math.max(getPercentageScore(score1), getPercentageScore(score2))
+}
 
 // Don't change anything below this line.
 module.exports = {
