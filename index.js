4f634b5832b1c45c4590df20ca397624f8992693
aardvarkpepper <aardvarkpepper@gmail.com>
2022-11-14 21:05:08 -0500

    Change getHighestOfTwoScores to use Math.max instead of ternary.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function getPercentageScore(score) {
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
 function getHighestOfTwoScores(score1, score2) {
-  return getPercentageScore(score1) > getPercentageScore(score2) ? getPercentageScore(score1): getPercentageScore(score2);
+  return Math.max(getPercentageScore(score1), getPercentageScore(score2));
 }
 
 // Don't change anything below this line.
