b61be51a37ea20532ad1d9d72375658e3dc8e823
Fig <bethanytrr@gmail.com>
2022-11-14 20:44:30 -0500

    Completed lab, all tests pass.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments)
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +14,9 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  return Math.round(score.received / score.max *100)
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +26,11 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let first = Math.round(score1.received / score1.max * 100)
+  let second = Math.round(score2.received / score2.max * 100)
+  return Math.max(first, second)
+}
 
 // Don't change anything below this line.
 module.exports = {
