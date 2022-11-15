7706be10bc3d00f620a67840fc5fb644d439e26d
shareekaepps <shareekaepps@icloud.com>
2022-11-14 22:44:33 -0500

    first func getAssignmentTypes done

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+// Example assignments data: 
+
+const assignments = {
+  quizzes: [
+    { title: "Arrays", score: { received: 4, max: 5 } },
+    { title: "Objects", score: { received: 5, max: 6 } },
+    { title: "Loops", score: { received: 2, max: 4 } },
+  ],
+  labs: [
+    { title: "Arrays", score: { received: 0, max: 1 } },
+    { title: "Objects", score: { received: 1, max: 1 } },
+    { title: "Loops", score: { received: 1, max: 1 } },
+  ],
+  projects: [
+    { title: "Music Box", score: { received: 14, max: 18 } },
+    { title: "Calculator", score: { received: 12, max: 12 } },
+  ],
+};
+
 /**
  * Returns all of the "categories" from the assignments object.
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  console.log(Object.keys(assignments))
+
+  return Object.keys(assignments);
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +35,10 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+
+  
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
