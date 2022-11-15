3113c2c38f95d85d6edae188b21e077d91e9db7c
arerimr <raydelys.morrobelr@gmail.com>
2022-11-14 21:04:53 -0500

    Built in Methods lab finished

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  let a;
+  a = Object.keys(assignments)
+  return a
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +16,16 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  let result = 0;
+  let round = 0
+  let max = score.max
+  let rec = score.received
+  result = ((rec/max) * 100)
+  round = Math.round(result)
+  //console.log(result)
+  return round
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +35,18 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let a;
+  let b;
+  let max = 0
+
+  a = getPercentageScore(score1)
+  b = getPercentageScore(score2)
+
+  max = Math.max(a, b)
+
+  return max
+}
 
 // Don't change anything below this line.
 module.exports = {
