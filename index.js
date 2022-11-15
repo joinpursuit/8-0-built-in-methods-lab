884a3c8f9b2ef8acb9f3adc525917e71c84a3793
jkpasaoa <jacqueline.pasaoa@gmail.com>
2022-11-14 21:29:43 -0500

    get function getHighestOfTwoScores. done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function getAssignmentTypes(assignments) {
  * @returns {number} A rounded integer, representing a percentage.
  */
 function getPercentageScore(score) {
- return Math.round((score.received/ score.max) * 100)
+ return Math.round((score.received / score.max) * 100)
 }
   // let max = Math.max(score)
   // let min = Math.min(score)
@@ -33,7 +33,9 @@ function getPercentageScore(score) {
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  return Math.max(getPercentageScore(score1), getPercentageScore(score2));
+}
 
 // Don't change anything below this line.
 module.exports = {
