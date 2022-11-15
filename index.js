8948b232fee2a875091f4a3443ac942f9132ad2b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-11-14 20:07:04 -0500

    Finished 3 questions

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+
 /**
  * Returns all of the "categories" from the assignments object.
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  return Object.keys(assignments)   // return all the categories (keys) - so we use Object.keys
+} // end of function
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +15,9 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+  return Math.round(( score.received / score.max ) * 100) // Calculate percentage and use Math.round to round the integer.
+} // end of function
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +27,14 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let percentScore1 = getPercentageScore(score1)     // to calculate percentage, calling the previous function which did same
+  let percentScore2 = getPercentageScore(score2)
+
+  return Math.max(percentScore1,percentScore2)       // now calculate the max of 2 percentage and return
+} //end of function
+
+
 
 // Don't change anything below this line.
 module.exports = {
