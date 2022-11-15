875899964bb08ea43c51442879084082b767f03f
shareekaepps <shareekaepps@icloud.com>
2022-11-14 22:46:54 -0500

    last two func done, had to re-fork, rename due to commit push issues

--- index.js
+++ index.js
@@ -36,8 +36,10 @@ function getAssignmentTypes(assignments) {
  * @returns {number} A rounded integer, representing a percentage.
  */
 function getPercentageScore(score) {
-
+  const percentageOfScore = (score.received / score.max * 100);
+  console.log(percentageOfScore)
   
+  return Math.round(percentageOfScore);
 }
 
 /**
@@ -48,7 +50,15 @@ function getPercentageScore(score) {
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  const firstScore = (score1.received / score1.max * 100);
+  const secondScore = (score2.received / score2.max * 100);
+
+  theHighestScore = (firstScore, secondScore)
+  console.log(theHighestScore);
+  
+  return Math.max(Math.round(theHighestScore));
+}
 
 // Don't change anything below this line.
 module.exports = {
